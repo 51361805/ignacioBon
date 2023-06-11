87603d6b23e98cf515c4cb508add35a9b2d64d6c
ignacio bon <nachitoo_bon@icloud.com>
2023-06-11 18:30:32 -0300

    dark

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 const verMasBtn = document.getElementById('ver-mas');
 const aboutSection = document.querySelector('.about-section');
 
-verMasBtn.addEventListener('click', function() {
-  aboutSection.classList.toggle('expand');
+verMasBtn.addEventListener('click' , function () {
+  
+aboutSection.classList.toggle('expand');
+
+})
+
+const sliderCheckbox = document.getElementById("slider-checkbox");
+const modoOscuro = document.getElementById('darkBody');
+const aplicarModo = document.querySelector('.dark');
+
+sliderCheckbox.addEventListener("change", function() {
+  if (this.checked) {
+    // El botón está activado
+    aplicarModo.classList.add('dark');
+  } else {
+    // El botón está desactivado
+    aplicarModo.classList.remove('dark');
+  }
 });
+
